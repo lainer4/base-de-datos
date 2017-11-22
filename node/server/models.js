3fb794193d570323c267afdd5fc1ea2d8426cd4a
jairo lainer moreno guevara <jmjlainer24@gmail.com>
2017-11-21 23:53:48 -0500

    final
    final

---
+++ node/server/models.js
@@ -0,0 +1,62 @@
+
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    autoIncrement = require('mongoose-auto-increment'),
+    connection = mongoose.createConnection('mongodb://localhost/NodeJSCalendar');
+
+autoIncrement.initialize(connection);
+
+
+
+var UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+UserSchema.plugin(autoIncrement.plugin, {
+    model: 'User',
+    startAt: 0
+});
+
+exports.User = connection.model('User', UserSchema);
+
+
+
+
+var EventSchema = new Schema({
+  userId: {
+    type: Number,
+    ref: 'User',
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  startDate: {
+    type: String,
+    required: true
+  },
+  startHour: String,
+  endDate: String,
+  endHour: String,
+  fullDay: {
+    type: Boolean,
+    required: true
+  }
+});
+
+EventSchema.plugin(autoIncrement.plugin, {
+  model: 'Event',
+  startAt: 0
+});
+
+exports.EventModel = connection.model('Event', EventSchema);
